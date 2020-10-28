3cf3b2d88b46862d9e993f1f8e7be074a17515ba
Thach <ducthach1401@gmail.com>
2020-10-28 22:49:15 +0700

    update

--- data/wordcount.py
+++ data/wordcount.py
@@ -1,13 +1,15 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark
-sc = SparkContext(appName="NetworkWordCount",master="spark://36a53dfcbce2:7077")
-ssc = StreamingContext(sc, 3)
-lines = ssc.textFileStream('./Data/')
+from pyspark.sql import SQLContext
+sc = SparkContext(appName="NetworkWordCount")
+ssc = StreamingContext(sc, 2)
+ssc.checkpoint("./tmp/checkspark")
+lines = ssc.socketTextStream("localhost",9999)
+# lines = ssc.textFileStream('./Data/')
 words = lines.flatMap(lambda line: line.split(" "))
 pairs = words.map(lambda word: (word, 1))
 wordCounts = pairs.reduceByKey(lambda x, y: x + y)
-
+# wordCounts = pairs.reduceByKeyAndWindow(lambda x,y:x+y,lambda x,y: x-y, 6,2)
 wordCounts.pprint(100)
 ssc.start() 
 ssc.awaitTermination()
